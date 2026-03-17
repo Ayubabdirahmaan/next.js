2ffb70e305b702ae19fe12cd6f16a58c84570021
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-03-17 22:33:45 +0300

    added page route

---
+++ app/Dashboard/layout.tsx
@@ -0,0 +1,8 @@
+export default function DashboardLayouts({children} : {children: React.ReactNode} ) {
+    return (
+        <div>
+            <h1>Dashboard Layouts</h1>
+            {children}
+        </div>
+    )
+}
