296887babc9d0370fe0317ee6eaa390a3fc142bb
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-03-18 23:12:57 +0300

    solve isseu

--- app/Dashboard/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardLayouts({children} : {children: React.ReactNode} ) {
-    return (
-        <div>
-            <h1>Dashboard Layouts</h1>
-            {children}
-        </div>
-    )
-}
