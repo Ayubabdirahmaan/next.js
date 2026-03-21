a107cffb4630f1ad4dbd4a9e2a4eefd05bbfda1f
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-03-21 21:24:55 +0300

    added routes

---
+++ app/Dashbaord/layout.tsx
@@ -0,0 +1,8 @@
+export default function LayoutDashboard({children}: {children: React.ReactNode}) {
+    return (
+        <div>
+            <h2>Layout Dashboard</h2>
+            {children}
+        </div>
+    )
+}
---
+++ app/Dashbaord/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashbaord = () => {
+  return (
+    <div>Dashbaord Page</div>
+  )
+}
+
+export default Dashbaord
