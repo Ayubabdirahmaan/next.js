9f3aaac5fd52d1698cf3da41a36ea2c8db7a2341
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-03-21 21:08:56 +0300

    added

--- app/Dashboard/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashbaordLayout({children}: {children: React.ReactNode}) {
-    return (
-        <div>
-            <p>Dashbaord Layout</p>
-            {children}
-        </div>
-    )
-}
