ca181d5b44681746fae7af4e761f2c205c792445
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-03-23 14:43:26 +0300

    added api

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,3 @@
-import Image from "next/image";
-
-export default  async function Home() {
-
-  const res = await fetch('https://dummyjson.com/products')
-  const result = await res.json()
-
-    // console.log(result);
-  return (
-   <div>
-
-    {
-      result.products?.map((pro: any) => (
-        <li key={pro.id}>{pro.title}</li>
-      ))
-    }
-   
-   </div>
-  );
+export default async function Home() {
+  return <div></div>;
 }
