35fcd318b8d2bbeed948b2f75e09e74160d8f834
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-03-29 23:43:04 +0300

    added

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
-"use client"
-import{ useActionState } from 'react'
-import { greet } from './form/action'
-const initialState = {
-    message : ''
-}
 const HomePage = () => {
-        const [state, FormData] = useActionState(greet, initialState)
-  return (
-    <form action={FormData}>
-        <input type="text" name='name' />
-        <button>submit</button>
-        {
-            state.message && (
-                <p>{state.message}</p>
-            )
-        }
-    </form>
-  )
-}
+  return <h1>hello world</h1>;
+};
 
-export default HomePage
+export default HomePage;
