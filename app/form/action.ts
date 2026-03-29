58fd2ab688c87d5e1234e60c0c434ac492210129
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-03-29 23:58:10 +0300

    added form data

--- app/form/action.ts
+++ app/form/action.ts
@@ -1,5 +1,12 @@
-export async function greet(formdata: FormData) {
-  const name = formdata.get("name");
+'use server'
 
-  return `hello ${name} from server`;
+type formSate = {
+    message : string
+}
+export async function greet(prevState: formSate, formdata: FormData) : Promise<formSate> {
+  const name = formdata.get("name")?.toString()
+        if(!name || name.trim() === "") {
+            return {message: 'form is required'}
+        }
+  return {message: `hello ${name} to server`}
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+"use client"
+import { useActionState } from "react";
+import { greet } from "./form/action";
+const initialState = {
+    message: ''
+}
 const HomePage = () => {
-  return <h1>hello world</h1>;
+    const [state, formData ] = useActionState(greet, initialState )
+  return (
+    <form action={formData}>
+        <input type="text" name="name" />
+        <button>submit</button>
+        {
+            state.message && (
+                <p>{state.message}</p>
+            )
+        }
+    </form>
+  )
 };
 
 export default HomePage;
