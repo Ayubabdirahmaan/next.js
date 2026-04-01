dc732cbfde96db74be95c4166f64bbc5476c3256
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-04-01 23:59:08 +0300

    added revalidate of fetching cach

--- app/Products/page.tsx
+++ app/Products/page.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { revalidateProduct } from './revalidate'
 
 const ProductPage = async () => {
     const res = await fetch('https://dummyjson.com/products', {
-        cache: 'no-store'
+       next: {tags: ['products']}
     })
     const data = await res.json()
     const timestemp = new Date().toLocaleTimeString()
   return (
     <div>
-        <p>🕒 timestemp at: {timestemp}</p>
+        <p>🕒 timestemp at: 20s fetching {timestemp}</p>
+        <button onClick={revalidateProduct}>🔄 Revalidate</button>
     </div>
   )
 }
