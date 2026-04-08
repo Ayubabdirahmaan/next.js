769198392fed4f18acd1b173f0406b1f0b6bce77
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-04-08 19:11:44 +0300

    added

--- app/Products/page.tsx
+++ app/Products/page.tsx
@@ -3,16 +3,16 @@ import { revalidateProduct } from './revalidate'
 
 const ProductPage = async () => {
     const res = await fetch('https://dummyjson.com/products', {
-       next: {tags: ['products']}
+        next: { tags: ['products'] }
     })
     const data = await res.json()
     const timestemp = new Date().toLocaleTimeString()
-  return (
-    <div>
-        <p>🕒 timestemp at: 20s fetching {timestemp}</p>
-        <button onClick={() => revalidateProduct}>🔄 Revalidate</button>
-    </div>
-  )
+    return (
+        <div>
+            <p>🕒 timestemp at: 20s fetching {timestemp}</p>
+            <button onClick={() => revalidateProduct}>🔄 Revalidate</button>
+        </div>
+    )
 }
 
 export default ProductPage
